4400fa6cd87ef2cb9a46e6c425cca19a90c99dd7
Boris Skip <boris.v.skip@gmail.com>
2019-12-12 13:56:57 +0200

    cleanup

--- UnitTests/Tests/testdata.py
+++
@@ -1,8 +0,0 @@
-'''Credentials and other testdata file'''
-
-# Home page of EventExpress App
-HOME = 'https://localhost:44364/home/events?page=1'
-
-# Credentials
-USERNAME = 'user@gmail.com'
-PASSWORD = '1qaz1qaz'
