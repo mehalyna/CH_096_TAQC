69910697895f61fe5f53a260444ac4266f259329
TarikOk <35561861+TarikOk@users.noreply.github.com>
2019-12-20 18:11:00 +0200

    fix_tests_tarasa

--- Tests/test_SearchPanelField.py
+++ Tests/test_SearchPanelField.py
@@ -1,6 +1,6 @@
 from Data.credentials import user,admin
 
-def test_login(app):
+def test_setup(app):
     app.signin.enter_actor(admin['email'],admin['password'])
 
 
--- Tests/test_SearchPanelFilter.py
+++ Tests/test_SearchPanelFilter.py
@@ -1,6 +1,6 @@
 from Data.credentials import user,admin
 
-def test_loginsetUp(app):
+def test_setup(app):
     app.signin.enter_actor(admin['email'],admin['password'])
 
 
