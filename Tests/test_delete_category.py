d12e5c075f5f14b3301cad2b6f37842c16468e09
OlexandrToloshnyak <57140993+OlexandrToloshnyak@users.noreply.github.com>
2019-12-28 14:37:32 +0200

    OK

--- Tests/test_delete_category.py
+++ Tests/test_delete_category.py
@@ -1,26 +1,28 @@
-from Tests.test_init import TestInit
 from Data.credentials import user,admin
+import pytest
+import allure
 from Data.test_data import CategoriesPage
+@allure.link("http://localhost:57690/admin/categories/", name='Click me')
+@allure.feature('Login User')
+@allure.story('Test deleting category')
+@allure.severity(allure.severity_level.CRITICAL)
+def test_delete_category(app,login_admin,screenshot_on_failure):
+    category_old = CategoriesPage.category_old
+    with allure.step("Go to Categories page."):
+        app.navigation.click_on_categories()
+    with allure.step("Creating Category."):
+        app.categories.add_category(category_old)
+        test1=app.categories.check_category_added(category_old)
+        assert (app.categories.check_category_added(category_old) == True), "Category was not created"
+    with allure.step("Deleting Category"):
+        app.categories.delete_category(category_old)
+        test2=app.categories.check_category_deleted(category_old)
+        assert (app.categories.check_category_deleted(category_old) == True), "Category was not created"
+    if test1 and test2:
+        test3=True
+    else:
+        test3=False
+    print(test3)
+    assert test3 , "Failed"
 
-
-
-
-
-
-class TestDeleteCategory(TestInit):
-
-
-    def setUp(self):
-        super().setUp()
-        self.category=CategoriesPage.category_old
-        self.exec.signin.enter_actor(admin['email'], admin['password'])
-        self.exec.navigation.click_on_categories()
-        self.exec.categories.add_category(self.category)
-
-
-
-    def test_delete_category(self):
-        self.exec.categories.check_category_added(self.category)
-        self.exec.categories.delete_category(self.category)
-        test=self.exec.categories.check_category_deleted(self.category)
-        self.assertTrue(test)
+    #teatdown
--- Tests/test_edit_category.py
+++ Tests/test_edit_category.py
@@ -1,24 +1,31 @@
 from Data.credentials import user,admin
+import pytest
+import allure
 from Data.test_data import CategoriesPage
-
-
-def test_edit_category(app):
+@allure.link("http://localhost:57690/admin/categories/", name='Click me')
+@allure.feature('Login User')
+@allure.story('Test editing category')
+@allure.severity(allure.severity_level.CRITICAL)
+def test_edit_category(app,login_admin,screenshot_on_failure):
     category_old = CategoriesPage.category_old
     category_new = CategoriesPage.category_new
-    app.signin.enter_actor(admin['email'], admin['password'])
-    app.navigation.click_on_categories()
-    app.categories.add_category(category_old)
-
-    test1=app.categories.check_category_deleted(category_new)
-    app.categories.edit_category(category_old, category_new)
-    app.categories.check_category_added(category_new)
-    test2=app.categories.check_category_added(category_new)
+    with allure.step("Go to Categories page."):
+        app.navigation.click_on_categories()
+    with allure.step("Creating Category."):
+        app.categories.add_category(category_old)
+        test1=app.categories.check_category_added(category_old)
+        assert (app.categories.check_category_added(category_old)==True) , "Category was not created"
+    with allure.step("Editing Category."):
+        app.categories.edit_category(category_old, category_new)
+        test2=app.categories.check_category_added(category_new)
+        assert (app.categories.check_category_added(category_new)==True) , "Category was not edited"
+    #test2=app.categories.check_category_added('Hello2')
     if test1 and test2:
         test3=True
     else:
         test3=False
     print (test3)
-    #self.assertTrue(test3)
+    assert test3 , "Failed"
 
     #teardown
     app.categories.delete_category(category_new)
