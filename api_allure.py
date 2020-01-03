37ba9d7510684e4ed84d3dee0fbc442601eaed80
TarikOk <35561861+TarikOk@users.noreply.github.com>
2020-01-03 17:42:06 +0200

    add new field to gitignor

--- api_allure.py
+++ api_allure.py
@@ -3,4 +3,4 @@ import requests
 allureServer = 'http://192.168.99.101:5050' #localhost
 
 def allure_api_get(api):
-    return requests.get(allureServer + '/'+ api')
+    return requests.get(allureServer + '/'+ api)
