2b4226903f3e24c805cee5ac6b49d5fa921a4a20
TarikOk <35561861+TarikOk@users.noreply.github.com>
2020-01-03 18:01:14 +0200

    update HOME_URL

--- Data/test_data.py
+++ Data/test_data.py
@@ -9,10 +9,11 @@ class Config:
     '''TEST_MODE set to True, runs rests in silent mode: no UI while testing'''
 
     TEST_MODE = False  # False
+    HOME_URL = "https://eventsexpress20200103054152.azurewebsites.net/home/events?page=1"
     # HOME_URL = "http://localhost:3183/home/events?page=1"
     # HOME_URL = "https://localhost:44364/home/events?page=1"  # Boris
     # HOME_URL = "http://localhost:3183/home/events?page=1"
-    HOME_URL = "http://localhost:49862/home/events?page=1" # Taras
+    #HOME_URL = "http://localhost:49862/home/events?page=1" # Taras
     # HOME_URL = "http://localhost:50621/home/events?page=1" # Masha
     # BROWSER = 'Chrome'
     BROWSER = 'Firefox'
