f95c0d72f288396f582427dcbf93b68ecf3a4003
Boris Skip <boris.v.skip@gmail>
2020-01-15 20:49:30 +0200

    PyLint corrections.

--- Data/test_data.py
+++ Data/test_data.py
@@ -1,27 +1,25 @@
 import random
 import os
 
-# from Locators.locators import ProfilePageEventsMenuLocators
-from selenium.webdriver.common.by import By
-
 
 current_path = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    '''TEST_MODE set to True, runs rests in silent mode: no UI while testing'''
+    """TEST_MODE set to True, runs rests in silent mode: no UI while testing"""
 
     TEST_MODE = False  # False
     HOME_URL = "https://eventsexpress20200103054152.azurewebsites.net/home/events?page=1"
     # HOME_URL = "http://localhost:3183/home/events?page=1"
     # HOME_URL = "https://localhost:44364/home/events?page=1"  # Boris
     # HOME_URL = "http://localhost:3183/home/events?page=1"
-    #HOME_URL = "http://localhost:49862/home/events?page=1" # Taras
+    # HOME_URL = "http://localhost:49862/home/events?page=1" # Taras
     # HOME_URL = "http://localhost:50621/home/events?page=1" # Masha
     BROWSER = 'Chrome'
     # BROWSER = 'Firefox'
 
 
-class CreateEventData():
+class CreateEventData:
 
 
     TITLE = random.choice( ['New Year', 'Christmas', 'Malanka'] )
--- Driver/driver.py
+++ Driver/driver.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
+
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import IEDriverManager
+
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 class Driver:
-    '''Provides automatically manage of drivers for different browsers'
+    """Provides automatically manage of drivers for different browsers'
     https://pypi.org/project/webdriver-manager/
 
     Installation:
@@ -15,14 +17,14 @@ class Driver:
     https://github.com/SergeyPirogov/webdriver_manager/tree/master
     NB: Opera browser does`t supported
 
-    Desired.Capabilities is going to be implemented soon'
-    '''
+    Desired.Capabilities is going to be implemented'
+    """
 
     def __init__(self, browser):
         self.browser = browser
 
     def set_browser(self, mode):
-        '''mode is set to True, runs rests in silent mode: no UI while testing'''
+        """mode is set to True, runs rests in silent mode: no UI while testing"""
 
         if self.browser.lower() == "firefox":
             options = FirefoxOptions()
@@ -38,10 +40,10 @@ class Driver:
             return webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
         elif self.browser.lower() == "ie":
-            '''The IE driver does not support execution without an active, 
-            logged-in desktop session running. You'll need to take this up with 
-            the author of the solution you're using to achieve "headless" 
-            (scare quotes intentional) execution of IE.'''
+            """The IE driver does not support execution without an active,
+            logged-in desktop session running. You'll need to take this up with
+            the author of the solution you're using to achieve 'headless'
+            (scare quotes intentional) execution of IE."""
             options = 'unsupported'
             return webdriver.Ie(IEDriverManager().install(), options=options)
 
