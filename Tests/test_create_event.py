acdf28225bec31a2ede6292d295c15fd51bd416e
TarikOk <35561861+TarikOk@users.noreply.github.com>
2020-01-17 16:28:35 +0200

    fix test create event

--- Tests/test_create_event.py
+++ Tests/test_create_event.py
@@ -9,23 +9,23 @@ from utilities.testLogging import PyLogging
 event = CreateEventData
 locator = CreateEvent
 
-@allure.link("http://localhost:3183/home/events?page=1")
+@allure.link("http://34.65.101.58:5002/home/events/?page=1")
 @allure.feature('Create Event')
 @allure.story("Create new event")
 def test_create_event(app):
     """Test sreate event"""
-    app.signin.enter_actor( user['email'], user['password'] )
-    app.navigation.click_on_profile( )
-    app.prof_menu.click_on_add_event( )
-    app.creat_event.upload_image( )
-    app.creat_event.add_title( event.TITLE )
+    app.signin.enter_actor(user['email'], user['password'])
+    app.navigation.click_on_profile()
+    app.prof_menu.click_on_add_event()
+    app.creat_event.upload_image()
+    app.creat_event.add_title(event.TITLE)
     text = event.DESCRIPTION['New Year']  # text add into description field for testing
-    app.creat_event.add_desc( text )
-    assert app.base.check_if_text_present( locator.DESC_TEXT, text )
-    app.base.click_on_element( locator.CATEGORY )
-    app.creat_event.add_category( locator.LST_CATEGORIES )
+    app.creat_event.add_desc(text)
+    assert app.base.check_if_text_present(locator.DESC_TEXT, text)
+    app.base.click_on_element(locator.CATEGORY)
+    app.creat_event.add_category(locator.LST_CATEGORIES)
     app.base.click_action(0, 0)
-    app.base.scroll_to_element( locator.COUNTRY_FIELD )
-    app.creat_event.select_country( locator.COUNTRY_FIELD )
-    app.base.click_on_element( locator.CITY )
+    app.base.scroll_to_element(locator.COUNTRY_FIELD)
+    app.creat_event.select_country(locator.COUNTRY_FIELD)
+    app.base.click_on_element(locator.CITY)
     app.creat_event.select_city(locator.CITY)
