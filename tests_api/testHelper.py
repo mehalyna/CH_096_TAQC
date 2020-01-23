0749bdf3de1b2f859420d46f5660f9a8787cb6fe
OlexandrToloshnyak <57140993+OlexandrToloshnyak@users.noreply.github.com>
2020-01-23 18:21:33 +0200

    Test User API

--- tests_api/testHelper.py
+++ tests_api/testHelper.py
@@ -52,20 +52,36 @@ class Header:
 
 class User:
     """Class to test Users API"""
-    def __init__(self, id="f320932e-aac2-4999-32d3-08d79b47df59", gender=0):
+
+    def __init__(
+            self,
+            id="f320932e-aac2-4999-32d3-08d79b47df59",
+            gender=0,
+            birthday="2000-01-01"):
         """Init new user data"""
         self.id = id
+        self.birthday = birthday + "T00:00:00"
         self.gender = gender
         self.payload_edit_gender = {
             "id": self.id,
             "gender": self.gender}
+        self.payload_edit_birthday = {
+            "id": self.id,
+            "birthday": self.birthday}
 
     def PAYLOAD_edit_gender(self):
-        """Reload PAYLOAD data"""
+        """Reload PAYLOAD_gender data"""
         PAYLOAD_edit_gender = {
             "id": self.id,
             "gender": self.gender}
         return PAYLOAD_edit_gender
+
+    def PAYLOAD_edit_birthday(self):
+        """Reload PAYLOAD_birthday data"""
+        PAYLOAD_edit_birthday = {
+            "id": self.id,
+            "birthday": self.birthday}
+        return PAYLOAD_edit_birthday
 
     def edit_gender(self):
         """Edit user gender"""
@@ -74,7 +90,16 @@ class User:
             data=json.dumps(
                 self.payload_edit_gender),
             headers=Header().get_header_auth_admin())
-        print("Є піпка")
+        print("Гендер змінено")
+
+    def edit_birthday(self):
+        """Edit user birthday"""
+        response_decoded_json = requests.post(
+            URL_USERS['edit_birthday'],
+            data=json.dumps(
+                self.payload_edit_birthday),
+            headers=Header().get_header_auth_admin())
+        print("Дату відредаговано на ", self.birthday[:10])
 
     def back_gender(self):
         """Return user data"""
@@ -84,7 +109,17 @@ class User:
             data=json.dumps(
                 self.PAYLOAD_edit_gender()),
             headers=Header().get_header_auth_admin())
-        print("Нема піпки")
+        print("Гендер вернувся")
+
+    def back_birthday(self):
+        """Return user birthday"""
+        self.birthday = "2000-01-01T00:00:00"
+        response_decoded_json = requests.post(
+            URL_USERS['edit_gender'],
+            data=json.dumps(
+                self.PAYLOAD_edit_birthday()),
+            headers=Header().get_header_auth_admin())
+        print("Дату повернено на ", self.birthday[:10])
 
     def get_info_by_id(self):
         """Get all user info by user-id"""
@@ -102,12 +137,25 @@ class User:
         gender = dictionary["gender"]
         return gender
 
-id="f320932e-aac2-4999-32d3-08d79b47df59"
+    def get_birthday(self):
+        """Get user birthday"""
+        dictionary = self.get_info_by_id()
+        birthday = dictionary["birthday"]
+        return birthday[:10]
+
+
+id = "f320932e-aac2-4999-32d3-08d79b47df59"
 print(Header().get_token_admin())
-user=User(id,1)
+user = User(id, 1, "2001-06-04")
+user.edit_birthday()
+# print(user.get_info_by_id())
+print(user.get_birthday())
+user.back_birthday()
+# print(user.get_info_by_id())
+print(user.get_birthday())
 user.edit_gender()
-print(user.get_info_by_id())
+# print(user.get_info_by_id())
 print(user.get_gender())
 user.back_gender()
-print(user.get_info_by_id())
+# print(user.get_info_by_id())
 print(user.get_gender())
