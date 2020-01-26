62e3f99b221c8b38533225ae51c9af049c90ec52
katherine <katya.pereverzeva2109@gmail.com>
2020-01-26 15:09:54 +0200

    change constants

--- tests_api/auth/test_auth.py
+++ tests_api/auth/test_auth.py
@@ -20,29 +20,29 @@ class TestAuth(unittest.TestCase):
         self.assertEqual(200, response_decoded_json.status_code, "You have BAD REQUEST")
 
     def test_login_user(self):
-        response_decoded_json = requests.post(URL_AUTH.url_login, data=json.dumps(AUTH_PAYLOADS.payload_user),
-                                              headers=HEADER.header)
+        response_decoded_json = requests.post(URL_AUTH['url_login'], data=json.dumps(AUTH_PAYLOADS['payload_user']),
+                                              headers=HEADER['header'])
         resp = response_decoded_json.json()
         print(resp["role"])
         self.assertEqual("User", resp["role"], "You don't login with correct role")
         self.assertEqual(200, response_decoded_json.status_code, "You have BAD REQUEST")
 
     def test_unauthorized_user(self):
-        response_decoded_json = requests.post(URL_AUTH.url_login, data=json.dumps(AUTH_PAYLOADS.payload_unauth),
-                                              headers=HEADER.header)
+        response_decoded_json = requests.post(URL_AUTH['url_login'], data=json.dumps(AUTH_PAYLOADS['payload_unauth']),
+                                              headers=HEADER['header'])
         mes = response_decoded_json.json()
         self.assertEqual(400, response_decoded_json.status_code, "You have BAD REQUEST")
 
     def test_register_already_exist(self):
-        response_decoded_json = requests.post(URL_AUTH.url_register, data=json.dumps(AUTH_PAYLOADS.payload_user),
-                                              headers=HEADER.header)
+        response_decoded_json = requests.post(URL_AUTH['url_register'], data=json.dumps(AUTH_PAYLOADS['payload_user']),
+                                              headers=HEADER['header'])
         mes = response_decoded_json.json()
         self.assertEqual("Email already exists in database", mes, "There is no such email in the database")
         self.assertEqual(400, response_decoded_json.status_code, "You have BAD REQUEST")
 
     def test_register_new_user(self):
-        response_decoded_json = requests.post(URL_AUTH.url_register, data=json.dumps(AUTH_PAYLOADS.payload_unauth),
-                                              headers=HEADER.header)
+        response_decoded_json = requests.post(URL_AUTH['url_register'], data=json.dumps(AUTH_PAYLOADS['payload_unauth']),
+                                              headers=HEADER['header'])
         self.assertEqual(200, response_decoded_json.status_code)
 
     @classmethod
