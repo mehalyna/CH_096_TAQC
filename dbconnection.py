8b3c08a13ea24c23a618bae4eb83874dcdae56db
Masha_Pertusiak <57170071+mashapetrusiak@users.noreply.github.com>
2020-01-27 17:48:22 +0200

    update connection

--- dbconnection.py
+++ dbconnection.py
@@ -11,7 +11,7 @@ class Connection:
         self.server = '34.65.101.58'
         self.database = 'EventsExpress'
         self.username = 'SA'
-        self.password = '$5Y6sJLLD5wq3$vY'
+        self.password = ''
         self.driver = 'ODBC Driver 17 for SQL Server'
         self.conn = pyodbc.connect('DRIVER=' + self.driver +
                                    ';SERVER=' + self.server +
