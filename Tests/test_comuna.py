c64f8f304c967cbdc70586397b22041f0d30d9be
katherine <katya.pereverzeva2109@gmail.com>
2020-01-28 22:16:21 +0200

    fix test for comuna with constants

--- Tests/test_comuna.py
+++ Tests/test_comuna.py
@@ -1,5 +1,6 @@
 import pytest
 import allure
+from config import INFO_MESSAGE_COMUNA as info
 
 
 @allure.link("http://34.65.101.58:5002/home/events?page=1", name='Click me')
@@ -11,7 +12,7 @@ def test_send_message(app):
     with allure.step("open communication page"):
         app.comuna.click_on_comuna()
         app.comuna.click_on_user_dialog()
-        app.comuna.type_message("hey")
+        app.comuna.type_message(info['message_to_sent'])
         app.comuna.click_send()
 
 
@@ -22,4 +23,4 @@ def test_send_message(app):
 @pytest.mark.usefixtures("login")
 def test_receive_message(app):
     app.comuna.click_on_comuna()
-    assert app.comuna.get_text_of_notify() == "You have 1 unread messages"
+    assert app.comuna.get_text_of_notify() == info['verify_message']
