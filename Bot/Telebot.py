cfb72db73e98f1c7a9031a3c7db3669d939ca879
OlexandrToloshnyak <57140993+OlexandrToloshnyak@users.noreply.github.com>
2020-01-30 23:11:59 +0200

    telebot

--- Bot/Telebot.py
+++ Bot/Telebot.py
@@ -1,11 +1,13 @@
 import telebot
-from tests_api.testHelper import User
 import dbconnection
 import datetime
+from tests_api.testHelper import User
+
 bot = telebot.TeleBot('821864551:AAH1yHj0Rvc3x9SZdGR3l-sKMX24l4vriGA')
-keyboard = telebot.types.ReplyKeyboardMarkup()
+keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
 keyboard.row('Привет', 'Пока', 'Тесты')
-keyboard_tests = telebot.types.ReplyKeyboardMarkup()
+keyboard_tests = telebot.types.ReplyKeyboardMarkup(
+    row_width=1, resize_keyboard=True)
 keyboard_tests.row('User', 'Category', 'Event', 'Back')
 keyboard_user = telebot.types.ReplyKeyboardMarkup(
     row_width=3, resize_keyboard=True)
@@ -28,11 +30,29 @@ keyboard_user.add(
     button_user7)
 keyboard_category = telebot.types.ReplyKeyboardMarkup(
     row_width=3, resize_keyboard=True)
-button_category1 = telebot.types.InlineKeyboardButton(text="category create_category Hello4")
-button_category2 = telebot.types.InlineKeyboardButton(text="category delete_category Hello4")
+button_category1 = telebot.types.InlineKeyboardButton(
+    text="category create_category Hello4")
+button_category2 = telebot.types.InlineKeyboardButton(
+    text="category delete_category Hello4")
+button_category3 = telebot.types.InlineKeyboardButton(
+    text="category edit_category Hello4 Hello5")
+button_category4 = telebot.types.InlineKeyboardButton(
+    text="category delete_category Hello5")
 keyboard_category.add(
     button_category1,
     button_category2,
+    button_category3,
+    button_category4,
+    button_user7)
+keyboard_event = telebot.types.ReplyKeyboardMarkup(
+    row_width=3, resize_keyboard=True)
+button_event1 = telebot.types.InlineKeyboardButton(
+    text="event create_event")
+button_event2 = telebot.types.InlineKeyboardButton(
+    text="event delete_event")
+keyboard_event.add(
+    button_event1,
+    button_event2,
     button_user7)
 users = [360913068, 290088874]
 @bot.message_handler(commands=['start'])
@@ -46,26 +66,18 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     user = User()
-    category = dbconnection.Connection()
+    db = dbconnection.Connection()
     if message.text.lower() == 'привет' and message.chat.id == 360913068:
         bot.send_message(message.chat.id, 'Привет мой господин!')
         bot.send_sticker(
             message.chat.id,
             'CAACAgIAAxkBAAMiXjF59f7yXbEXskvdAsHOymOFPDIAAsUCAAJcKIYI81ocvhJ5Qi4YBA')
     elif message.text.lower() == 'привет':
-        if message.chat.id == 290088874:
-            bot.send_message(
-                message.chat.id, 'Привет, пупсик')
-        else:
-            bot.send_message(
-                message.chat.id, 'Привет, {}'.format(
-                    message.from_user.first_name))
+        bot.send_message(
+            message.chat.id, 'Привет, {}'.format(
+                message.from_user.first_name))
     elif message.text.lower() == 'пока':
-        if message.chat.id == 290088874:
-            bot.send_message(
-                message.chat.id, 'Прощай, пупсик')
-        else:
-            bot.send_message(message.chat.id, 'Прощай, путник')
+        bot.send_message(message.chat.id, 'Прощай, путник')
     elif message.text.lower() == 'я тебя люблю':
         bot.send_video(
             message.chat.id,
@@ -92,7 +104,7 @@ def send_text(message):
                 print(user.get_username())
                 bot.send_message(message.chat.id, user.get_username())
             elif message.text.lower().split()[1] == 'back_username':
-                user.back_username()
+                user.edit_username("Vasya")
                 print(user.get_username())
                 bot.send_message(message.chat.id, user.get_username())
             elif message.text.lower().split()[1] == 'edit_birthday':
@@ -100,7 +112,7 @@ def send_text(message):
                 print(user.get_birthday())
                 bot.send_message(message.chat.id, user.get_birthday())
             elif message.text.lower().split()[1] == 'back_birthday':
-                user.back_birthday()
+                user.edit_birthday('2001-01-01')
                 print(user.get_birthday())
                 bot.send_message(message.chat.id, user.get_birthday())
             elif message.text.lower().split()[1] == 'edit_gender':
@@ -108,7 +120,7 @@ def send_text(message):
                 print(user.get_gender())
                 bot.send_message(message.chat.id, user.get_gender())
             elif message.text.lower().split()[1] == 'back_gender':
-                user.back_gender()
+                user.edit_gender(0)
                 print(user.get_gender())
                 bot.send_message(message.chat.id, user.get_gender())
         except BaseException:
@@ -121,23 +133,49 @@ def send_text(message):
         try:
             print(message.text.lower().split()[1])
             if message.text.lower().split()[1] == 'create_category':
-                category.create_category_with_name(message.text.split()[2])
+                db.create_category_with_name(message.text.split()[2])
                 print('Категория {} создана'.format(message.text.split()[2]))
                 bot.send_message(message.chat.id, message.text.split()[2])
             elif message.text.lower().split()[1] == 'delete_category':
-                category.delete_category_with_name(message.text.split()[2])
+                db.delete_category_with_name(message.text.split()[2])
                 print('Категория {} удалена'.format(message.text.split()[2]))
-                bot.send_message(message.chat.id, 'Category {} was deleted'.format(message.text.split()[2]))
+                bot.send_message(
+                    message.chat.id,
+                    'Category {} was deleted'.format(
+                        message.text.split()[2]))
             elif message.text.lower().split()[1] == 'edit_category':
-                user.edit_birthday(message.text.split()[2])
-                print(user.get_birthday())
-                bot.send_message(message.chat.id, user.get_birthday())
+                db.edit_category_with_name(
+                    message.text.split()[2], message.text.split()[3])
+                print(
+                    'Категория {} изменена на {}'.format(
+                        message.text.split()[2],
+                        message.text.split()[3]))
+                bot.send_message(
+                    message.chat.id,
+                    'Category {} was edited to {}'.format(
+                        message.text.split()[2],
+                        message.text.split()[3]))
         except BaseException:
             bot.send_message(
                 message.chat.id,
                 'Category:',
                 reply_markup=keyboard_category)
-
+    elif message.text.lower().split(' ')[0] == 'event':
+        try:
+            print(message.text.lower().split()[1])
+            if message.text.lower().split()[1] == 'create_event':
+                db.create_event()
+                print('Ивент был создан')
+                bot.send_message(message.chat.id, 'Event was created')
+            elif message.text.lower().split()[1] == 'delete_event':
+                db.delete_event_with_name()
+                print('Ивент был удалён')
+                bot.send_message(message.chat.id, 'Event was deleted')
+        except BaseException:
+            bot.send_message(
+                message.chat.id,
+                'Event:',
+                reply_markup=keyboard_event)
     else:
         bot.send_message(message.chat.id, 'оу , шо є ?!')
 
