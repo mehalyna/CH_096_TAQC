1042253df615b3d138cf2da9bc3e54bfe4efbfd5
katherine <katya.pereverzeva2109@gmail.com>
2020-02-02 17:04:22 +0200

    rename

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,10 +2,10 @@ import pytest
 import allure
 from utilities.testLogging import loger
 from allure_commons.types import AttachmentType
-from driver.driver import Driver
+from Driver.driver import Driver
 from config import CREDENTIALS, URL,CATEGORIESPAGE
 
-from pages.init_pages import InitPages
+from Pages.init_pages import InitPages
 from dbconnection import Connection
 
 
