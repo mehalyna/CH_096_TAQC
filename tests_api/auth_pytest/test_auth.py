444428602afa0ad1c499c7658e57c48d5eecc7f7
TarikOk <35561861+TarikOk@users.noreply.github.com>
2020-02-03 02:00:36 +0200

    edit test_auth

--- tests_api/auth_pytest/test_auth.py
+++ tests_api/auth_pytest/test_auth.py
@@ -1,20 +1,26 @@
+"""
+Api tests for login and registration user
+"""
 import json
-
 import allure
 import requests
 import pytest
-
 from tests_api.config import URL_AUTH, AUTH_PAYLOADS, HEADER
 
 
 @pytest.mark.usefixtures('delete_user_and_close_conn')
 class TestAuth():
+
     @allure.feature("Login admin api")
     @allure.story('Admin have an ability to login in EventExpress site')
     @allure.severity(allure.severity_level.CRITICAL)
     def test_login_admin(self):
-        response_decoded_json = requests.post(URL_AUTH['url_login'], data=json.dumps(AUTH_PAYLOADS['payload_admin']),
-                                              headers=HEADER['header'])
+        """
+        Test for login as admin
+        """
+        response_decoded_json = requests.post(URL_AUTH['url_login'], 
+                                            data=json.dumps(AUTH_PAYLOADS['payload_admin']),
+                                            headers=HEADER['header'])
         resp = response_decoded_json.json()
         assert "Admin" == resp["role"], "You don't login with correct role"
         assert 200 == response_decoded_json.status_code, "You have BAD REQUEST"
@@ -23,8 +29,12 @@ class TestAuth():
     @allure.story('User have an ability to login in EventExpress site')
     @allure.severity(allure.severity_level.CRITICAL)
     def test_login_user(self):
-        response_decoded_json = requests.post(URL_AUTH['url_login'], data=json.dumps(AUTH_PAYLOADS['payload_user']),
-                                              headers=HEADER['header'])
+        """
+        Test for login as autorize user
+        """
+        response_decoded_json = requests.post(URL_AUTH['url_login'], 
+                                            data=json.dumps(AUTH_PAYLOADS['payload_user']),
+                                            headers=HEADER['header'])
         resp = response_decoded_json.json()
         assert "User" == resp["role"], "You don't login with correct role"
         assert 200 == response_decoded_json.status_code, "You have BAD REQUEST"
@@ -32,17 +42,27 @@ class TestAuth():
     @allure.feature("Login as unauthorized api")
     @allure.story('Unauthorized user does not have an ability to login in EventExpress site')
     @allure.severity(allure.severity_level.CRITICAL)
+
     def test_unauthorized_user(self):
-        response_decoded_json = requests.post(URL_AUTH['url_login'], data=json.dumps(AUTH_PAYLOADS['payload_unauth']),
-                                              headers=HEADER['header'])
+        """
+        Test for login as notautorize user
+        """
+        response_decoded_json = requests.post(URL_AUTH['url_login'], 
+                                            data=json.dumps(AUTH_PAYLOADS['payload_unauth']),
+                                            headers=HEADER['header'])
         mes = response_decoded_json.json()
         assert 400 == response_decoded_json.status_code, "You have BAD REQUEST"
         assert "User not found" == mes, "There is unexpected ability to login as unknown user"
 
     @allure.feature("Register new user api")
-    @allure.story('User that is previously registered does not have an ability to register in EventExpress site')
+    @allure.story('User that is previously registered does not have\
+                    an ability to register in EventExpress site')
     @allure.severity(allure.severity_level.CRITICAL)
+
     def test_register_already_exist(self):
+        """
+        Test for registration user that already exists
+        """
         response_decoded_json = requests.post(URL_AUTH['url_register'],
                                               data=json.dumps(AUTH_PAYLOADS['payload_user']),
                                               headers=HEADER['header'])
@@ -53,7 +73,11 @@ class TestAuth():
     @allure.feature("Register new user api")
     @allure.story('Every new user have an ability to register in EventExpress site')
     @allure.severity(allure.severity_level.CRITICAL)
+
     def test_register_new_user(self):
+        """
+        Test for registration new user
+        """
         response_decoded_json = requests.post(URL_AUTH['url_register'],
                                               data=json.dumps(AUTH_PAYLOADS['payload_unauth']),
                                               headers=HEADER['header'])
